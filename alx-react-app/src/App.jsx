e3ff736cf5c4a553a41024e6e506d3e2996a85b9
kojolowlow1 <bryanahwireng10@gmail.com>
2026-01-11 23:42:09 +0000

    Add user profile to project

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,5 +1,6 @@
 import Header from './components/Header';
 import MainContent from './components/MainContent';
+import UserProfile from './components/UserProfile';
 import Footer from './components/Footer';
 
 function App() {
@@ -7,9 +8,23 @@ function App() {
     <div>
       <Header />
       <MainContent />
+      
+      <UserProfile 
+        name="Alice" 
+        age={25} 
+        bio="Loves hiking and photography" 
+      />
+      
+      <UserProfile 
+        name="Bob" 
+        age={30} 
+        bio="Enjoys cooking and traveling" 
+      />
+      
       <Footer />
     </div>
   );
 }
 
 export default App;
+
